76b827185c2c56a8f2a6fc4cb062d989a98b5064
ANUP PRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-10-10 15:25:34 +0530

    Create Lab5_question1.cpp

---
+++ Lab5_question1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main() {
+	int arr[20],a,b,sum=0;
+	cout<<" how many elements you want to enter ";
+	cin>>b;
+	cout<<" enter any "<<b<<" elements in array ";
+	for(a=0;a<b;a++)
+	{
+		cin>>arr[a];
+		} 
+		cout<<" sum of all the array elements are ";
+		for(a=0;a<b;a++)
+		{ 
+			sum=sum+arr[i];
+			}
+			for(a=0;a<b;a++)
+			{
+				}
+				cout<<sum;
+				}
